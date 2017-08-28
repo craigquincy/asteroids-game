060d039c37ff41bd5ee06432f45c8af7ef6957a1
Craig Quincy <craig.quincy@galvanize.com>
2017-08-28 14:00:41 -0600

    part 2, bullets

--- js/game.js
+++ js/game.js
@@ -40,12 +40,23 @@ var shipProperties = {
   angularVelocity: 200
 };
 
+var bulletProperties = {
+  speed: 400,
+  interval: 250,
+  lifespan: 2000,
+  maxCount: 30
+}
+
 var gameState = function(game) {
   this.shipSprite
 
   this.key_left;
   this.key_right;
   this.key_thrust;
+  this.key_fire;
+
+  this.bulletGroup;
+  this.bulletInterval = 0;
 };
 
 gameState.prototype = {
@@ -68,12 +79,16 @@ gameState.prototype = {
 
   update: function() {
     this.checkPlayerInput()
+    this.checkBoundaries(this.shipSprite);
+    this.bulletGroup.forEachExists(this.checkBoundaries, this);
+
   },
 
   initGraphics: function() {
     this.shipSprite = game.add.sprite(shipProperties.startX, shipProperties.startY, graphicAssets.ship.name);
     this.shipSprite.angle = -90;
     this.shipSprite.anchor.set(0.5, 0.5);
+    this.bulletGroup = game.add.group();
   },
 
   initPhysics: function() {
@@ -82,15 +97,25 @@ gameState.prototype = {
     game.physics.enable(this.shipSprite, Phaser.Physics.ARCADE);
     this.shipSprite.body.drag.set(shipProperties.drag);
     this.shipSprite.body.maxVelocity.set(shipProperties.maxVelocity);
+
+    this.bulletGroup.enableBody = true;
+    this.bulletGroup.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bulletGroup.createMultiple(bulletProperties.maxCount, graphicAssets.bullet.name);
+    this.bulletGroup.setAll('anchor.x', 0.5);
+    this.bulletGroup.setAll('anchor.y', 0.5);
+    this.bulletGroup.setAll('lifespan', bulletProperties.lifespan);
   },
 
   initKeyboard: function() {
     this.key_left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     this.key_right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     this.key_thrust = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    this.key_fire = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
   },
 
   checkPlayerInput: function() {
+    // rotate
     if (this.key_left.isDown) {
       this.shipSprite.body.angularVelocity = -shipProperties.angularVelocity;
     } else if (this.key_right.isDown) {
@@ -99,13 +124,51 @@ gameState.prototype = {
       this.shipSprite.body.angularVelocity = 0;
     }
 
+    // thrust
     if (this.key_thrust.isDown) {
       game.physics.arcade.accelerationFromRotation(this.shipSprite.rotation, shipProperties.acceleration, this.shipSprite.body.acceleration);
     } else {
       this.shipSprite.body.acceleration.set(0);
     }
-  }
 
+    // bullet
+    if (this.key_fire.isDown) {
+      this.fire();
+    }
+  },
+
+  checkBoundaries: function(sprite) {
+    if (sprite.x < 0) {
+      sprite.x = game.width;
+    } else if (sprite.x > game.width) {
+      sprite.x = 0;
+    }
+
+    if (sprite.y < 0) {
+      sprite.y = game.height;
+    } else if (sprite.y > game.height) {
+      sprite.y = 0;
+    }
+  },
+
+  fire: function() {
+    if (game.time.now > this.bulletInterval) {
+      var bullet = this.bulletGroup.getFirstExists(false);
+
+      if (bullet) {
+        var length = this.shipSprite.width * 0.5;
+        var x = this.shipSprite.x + (Math.cos(this.shipSprite.rotation) * length);
+        var y = this.shipSprite.y + (Math.sin(this.shipSprite.rotation) * length);
+
+        bullet.reset(x, y);
+        bullet.lifespan = bulletProperties.lifespan;
+        bullet.rotation = this.shipSprite.rotation;
+
+        game.physics.arcade.velocityFromRotation(this.shipSprite.rotation, bulletProperties.speed, bullet.body.velocity);
+        this.bulletInterval = game.time.now + bulletProperties.interval;
+      }
+    }
+  }
 };
 
 var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
